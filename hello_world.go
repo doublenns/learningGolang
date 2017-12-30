4e4b87000d3c12849fdc5c1889c50cd4e0e354b9
DoubleNNs <nnamdianyanwu@gmail.com>
2017-12-30 17:20:45 -0600

    os.Exit() needs exit codes. 2 types of comments

--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,17 @@
 package main
 
 import "fmt"
+import "os"
+
+// This is a single line Go comment
+
+/*
+This is a
+multi-line Go comment
+*/
 
 func main() {
     fmt.Println("Whaddup doe!!")
-    }
+    fmt.Println("Hello World"[1])
+    os.Exit(1)
+}
