6d9927eda7cdde2f7789eace5f1b7e040174e8db
DoubleNNs <nnamdianyanwu@gmail.com>
2017-12-30 18:41:48 -0600

    Multiple variable assignment

--- numbers.go
+++ numbers.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Please enter number a number to be doubled:")
-    var inputNum float64
-    fmt.Scanf("%f", &inputNum)
+    fmt.Println(`Please enter 2 numbers to both be doubled.
+    Number 1:`)
+    var (
+        inputNum1 float64
+        inputNum2 float64
+    )
+    fmt.Scanf("%f", &inputNum1)
+    fmt.Println("   Number2:")
+    fmt.Scanf("%f", &inputNum2)
 
-    outputNum := inputNum * 2
-    fmt.Println(outputNum)
+    outputNum1 := inputNum1 * 2
+    outputNum2:= inputNum2 * 2
+    fmt.Println(outputNum1)
+    fmt.Println(outputNum2)
 }
