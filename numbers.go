170eaebf1b4ad090a5e48c874ff44d6832dd3a01
DoubleNNs <nnamdianyanwu@gmail.com>
2017-12-31 15:22:56 -0600

    Iterate through values in array

--- numbers.go
+++ numbers.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Please enter 2 numbers to both be doubled.")
-    fmt.Println("Number 1:")
-
     var inputNums [2]float64
 
+    fmt.Println("Please enter 2 numbers to both be doubled.")
+    fmt.Println("Number 1:")
     fmt.Scanf("%f", &inputNums[0])
     fmt.Println("Number2:")
     fmt.Scanf("%f", &inputNums[1])
 
     fmt.Println("Numbers doubled:")
-    for i := 0; i <= 1; i++ {
-        fmt.Println(inputNums[i] * 2)
+    for _, num := range inputNums  {
+        fmt.Println(num * 2)
     }
 
     //for i in (outputNum1, outputNum2) {
