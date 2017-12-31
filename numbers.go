d73478e39ea2ab2597adc21fde4c2a47f0be7372
DoubleNNs <nnamdianyanwu@gmail.com>
2017-12-31 15:13:29 -0600

    Replaced var multiple assignment w/ array

--- numbers.go
+++ numbers.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println(`Please enter 2 numbers to both be doubled.
-    Number 1:`)
-    var (
-        inputNum1 float64
-        inputNum2 float64
-    )
-    fmt.Scanf("%f", &inputNum1)
-    fmt.Println("   Number2:")
-    fmt.Scanf("%f", &inputNum2)
+    fmt.Println("Please enter 2 numbers to both be doubled.")
+    fmt.Println("Number 1:")
 
-    outputNum1 := inputNum1 * 2
-    outputNum2:= inputNum2 * 2
-    fmt.Println(outputNum1)
-    fmt.Println(outputNum2)
+    var inputNums [2]float64
+
+    fmt.Scanf("%f", &inputNums[0])
+    fmt.Println("Number2:")
+    fmt.Scanf("%f", &inputNums[1])
+
+    fmt.Println("Numbers doubled:")
+    for i := 0; i <= 1; i++ {
+        fmt.Println(inputNums[i] * 2)
+    }
+
+    //for i in (outputNum1, outputNum2) {
+    //    fmt.Println(i)
+    //}
 }
