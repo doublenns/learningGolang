e005f1baa459b6616f267eead4620f4980329e84
DoubleNNs <nnamdianyanwu@gmail.com>
2018-04-14 00:10:15 -0500

    Added functions to deck type

--- courses/Go-The_Complete_Developers_Guide/cards/deck.go
+++ courses/Go-The_Complete_Developers_Guide/cards/deck.go
@@ -1,6 +1,29 @@
 package main
 
+import "fmt"
+
 // Create a new type of "deck"
 // that is a slice of strings
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for i, suits := range cardSuits {
+		for j, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
