c230548c61d5a178bbecde41615a1eb4bc0fb0ba
DoubleNNs <nnamdianyanwu@gmail.com>
2018-04-13 23:37:37 -0500

    Creating types - Go isn't OO

--- courses/Go-The_Complete_Developers_Guide/cards/main.go
+++ courses/Go-The_Complete_Developers_Guide/cards/main.go
@@ -6,10 +6,12 @@ import (
 
 func main() {
 
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	fmt.Println(cards)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
