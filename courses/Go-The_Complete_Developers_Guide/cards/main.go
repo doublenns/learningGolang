2afe96a89762176d4c196bcd51d0401deac66d74
DoubleNNs <nnamdianyanwu@gmail.com>
2018-04-13 22:26:03 -0500

    A function doesn't have to be defined 1st b4 being called in main()

--- courses/Go-The_Complete_Developers_Guide/cards/main.go
+++ courses/Go-The_Complete_Developers_Guide/cards/main.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 )
 
-func newCard() string {
-
-	return "Ace of Spades"
-
-}
-
 func main() {
 	// var card string = "Ace of Spades"
 
 	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+
+	return "Ace of Spades"
+
 }
