7ff3e74c22a5fe85486da71754b0107da3e89d11
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-22 13:29:59 -0500

    Add exampleof nested/embedded structs

--- courses/Go-The_Complete_Developers_Guide/structs/main.go
+++ courses/Go-The_Complete_Developers_Guide/structs/main.go
@@ -2,20 +2,30 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// alex := person{firstName: "Alex", lastName: "Anderson"}
-	// fmt.Println(alex)
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	var alex person
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 90210,
+		},
+	}
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-
-	fmt.Println(alex)
-	fmt.Printf("%+v\n", alex)
+	fmt.Printf("%+v\n", jim)
 }
