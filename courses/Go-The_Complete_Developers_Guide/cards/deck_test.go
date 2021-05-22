0a5d56429d6f4079deebc0ed90b9938942b601b2
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-21 22:26:26 -0500

    Add more tests for newDeck func

--- courses/Go-The_Complete_Developers_Guide/cards/deck_test.go
+++ courses/Go-The_Complete_Developers_Guide/cards/deck_test.go
@@ -9,4 +9,11 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
 
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card to be King of Clubs, but got %v", d[len(d)-1])
+	}
 }
