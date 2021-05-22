e058d87a4e46e03966db8dd4a1159ee0062972c9
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-22 13:37:39 -0500

    Examples of func that takes a struct as a recceiver

--- courses/Go-The_Complete_Developers_Guide/structs/main.go
+++ courses/Go-The_Complete_Developers_Guide/structs/main.go
@@ -10,7 +10,7 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
@@ -21,11 +21,20 @@ func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Party",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@gmail.com",
 			zipCode: 90210,
 		},
 	}
 
-	fmt.Printf("%+v\n", jim)
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
 }
