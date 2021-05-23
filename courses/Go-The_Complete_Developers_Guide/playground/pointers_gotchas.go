95871377d7086d0aaf026bffd495f7b4b839b1a8
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-22 19:25:39 -0500

    Everything in Go is pass by value -- even pointers

--- courses/Go-The_Complete_Developers_Guide/playground/pointers_gotchas.go
+++ courses/Go-The_Complete_Developers_Guide/playground/pointers_gotchas.go
@@ -13,8 +13,18 @@ func main() {
 
 	updateSlice(mySlice)
 	fmt.Println(mySlice)
+
+	name := "doublenns"
+	namePointer := &name
+
+	fmt.Println(&namePointer)
+	printPointer(namePointer)
 }
 
 func updateSlice(s []string) {
 	s[0] = "Bye"
+}
+
+func printPointer(namePointer *string) {
+	fmt.Println(&namePointer)
 }
