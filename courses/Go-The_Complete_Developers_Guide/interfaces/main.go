bceb622a6b2501a506c695078c893e5f6dde91a3
doublenns <nnamdi.anyanwu@gmail.com>
2021-05-22 21:51:19 -0500

    Example of using Go interface

--- courses/Go-The_Complete_Developers_Guide/interfaces/main.go
+++ courses/Go-The_Complete_Developers_Guide/interfaces/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -10,22 +14,19 @@ func main() {
 	sb := spanishBot{}
 
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 }
 
 func (englishBot) getGreeting() string {
 	// VERY custom logic for generating an English greeting
 	return "Whaddup!"
 }
+
 func (spanishBot) getGreeting() string {
 	// VERY custom logic for generating a Spanish greeting
 	return "Que paso!"
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
-
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting())
-// }
