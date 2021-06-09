3371b9129f746ba559dc1f3bc8b0d7945851fce5
doublenns <nnamdi.anyanwu@gmail.com>
2021-06-09 10:20:05 -0500

    Program that averages array elements returned from function/package

---
+++ books/Head_First-Go/average/main.go
@@ -0,0 +1,21 @@
+// average calculates the average of several numbers
+package main
+
+import (
+	"datafile"
+	"fmt"
+	"log"
+)
+
+func main() {
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	sampleCount := float64(len(numbers))
+	fmt.Println("Average: %0.2f\n", sum/sampleCount)
+}
