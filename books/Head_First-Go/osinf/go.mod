32f2544a07650fd568743515a623c552cfe277f3
doublenns <nnamdi.anyanwu@gmail.com>
2021-06-09 21:58:36 -0500

    Practice using go modules to import modules on local disk outside of GOROOT/GOPATH

---
+++ books/Head_First-Go/osinf/go.mod
@@ -0,0 +1,5 @@
+module main
+
+require example.com/me/pretty v0.0.0
+
+replace example.com/me/pretty => ../pretty
---
+++ books/Head_First-Go/osinf/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "example.com/me/pretty"
+
+func main() {
+	pretty.Pretty("hi")
+	pretty.Pretty([]int{1, 3, 5})
+}
