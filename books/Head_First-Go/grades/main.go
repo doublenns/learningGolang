36f95d05fec0e3f44787610aad95ba5e11af993f
doublenns <nnamdi.anyanwu@gmail.com>
2021-06-10 00:33:14 -0500

    Map zero values vs assigned 0; using the 'comma ok idiom'

---
+++ books/Head_First-Go/grades/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func status(name string) {
+	grades := map[string]float64{"Alma": 0, "Rohit": 86.5}
+	grade, ok := grades[name]
+	if !ok {
+		fmt.Printf("No grades recorded for %s.\n", name)
+	} else if grade < 60 {
+		fmt.Printf("%s is failing!\n", name)
+	} else {
+		fmt.Println("%s is passing.\n", name)
+	}
+}
+
+func main() {
+	status("Alma")
+	status("Carl")
+}
