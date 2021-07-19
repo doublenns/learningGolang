7469af0d379150355542bb0439d54498a7c0377b
doublenns <nnamdi.anyanwu@gmail.com>
2021-07-18 22:13:57 -0500

    Use Lambda Go pkg's events.APIGatewayProxyResponse struct for API GW integration

--- books/Hands-on_Serverless_Applications_with_Go/lambda-hello_world/main.go
+++ books/Hands-on_Serverless_Applications_with_Go/lambda-hello_world/main.go
@@ -1,14 +1,24 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
-type Response struct {
-	StatusCode int    `json:"statusCode"`
-	Body       string `json:"body"`
-}
+// type Response struct {
+// 	StatusCode int    `json:"statusCode"`
+// 	Body       string `json:"body"`
+// }
 
-func handler() (Response, error) {
-	return Response{
+// func handler() (Response, error) {
+// 	return Response{
+// 		StatusCode: 200,
+// 		Body:       "Welcome to Serverless World",
+// 	}, nil
+// }
+
+func handler() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Welcome to Serverless World",
 	}, nil
